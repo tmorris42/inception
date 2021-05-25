0c9de3a132445201ebe7ebd2422d13d6133cc62d
tmorris <tmorris@student.42.fr>
2021-05-25 14:20:01 +0200

    Fix database connection, wordpress path/urls problematic

--- srcs/requirements/mariadb/entrypoint.sh
+++ srcs/requirements/mariadb/entrypoint.sh
@@ -1,12 +1,12 @@
 service mysql start;
+DB_EXISTS=`mysql -Be "show databases;" | grep "$MYSQL_DATABASE" | wc -l`;
+if [ "$DB_EXISTS" = "0" ]; then
+	mysql -e "CREATE DATABASE $MYSQL_DATABASE;";
+fi
 USER_EXISTS=`mysql -Be "SELECT user FROM mysql.user;" | grep "$MYSQL_USER" | wc -l`;
 if [ "$USER_EXISTS" = "0" ]; then
 	mysql -e "CREATE USER '$MYSQL_USER'@'srcs_wordpress_1.srcs_default' IDENTIFIED BY '$MYSQL_PASSWORD';";
-	mysql -e "GRANT ALL PRIVILEGES ON * . * TO  '$MYSQL_USER'@'srcs_wordpress_1.srcs_default' IDENTIFIED BY '$MYSQL_PASSWORD';";
+	mysql -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE . * TO  '$MYSQL_USER'@'srcs_wordpress_1.srcs_default' IDENTIFIED BY '$MYSQL_PASSWORD';";
 	mysql -e "FLUSH PRIVILEGES;";
-fi
-DB_EXISTS=`mysql -Be "show databases;" | grep "$MYSQL_DATABASE" | wc -l`;
-if [ "$DB_EXISTS" = "0" ]; then
-	mysql -u $MYSQL_USER --password="$MYSQL_PASSWORD" -e "CREATE DATABASE $MYSQL_DATABASE;";
 fi
 mysqld;
--- srcs/requirements/wordpress/srcs/setup.sh
+++ srcs/requirements/wordpress/srcs/setup.sh
@@ -2,16 +2,18 @@ service php7.3-fpm start
 service php7.3-fpm stop
 
 cp /tmp/html /var/www/
+cp /tmp/wp-config.php /var/www/html/
+#cp /tmp/wp-config.php /var/www/html/wordpress/
 
 # Set permissions
 #chown www-data:www-data /var/www/tmorris/html/phpmyadmin/tmp
 #chmod 700 /var/www/tmorris/html/phpmyadmin/tmp
-#chmod +x /usr/local/bin/wp
+chmod +x /usr/local/bin/wp
 
 # Install WordPress using non-root user
-#useradd -m subuser
-#chown -R subuser:subuser /var/www/tmorris/html/wordpress
-#runuser -l subuser -c 'wp core install --url="localhost/wordpress/"  --title="tmorris Demo" --admin_user="admin" --admin_password="user42" --admin_email="example@example.com" --path=/var/www/tmorris/html/wordpress'
+useradd -m subuser
+chown -R subuser:subuser /var/www/html/
+runuser -l subuser -c 'wp core install --url="localhost/"  --title="tmorris Demo" --admin_user="admin" --admin_password="user42" --admin_email="example@example.com" --path=/var/www/html/'
 
 php-fpm7.3 -F -R
 
