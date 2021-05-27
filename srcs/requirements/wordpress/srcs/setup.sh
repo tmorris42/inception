1307fc3ec9cd23497bdbdbcb6fb31c8bb01b47e5
tmorris <tmorris@student.42.fr>
2021-05-27 09:31:51 +0200

    Fix wordpress URL issues

--- srcs/requirements/wordpress/srcs/setup.sh
+++ srcs/requirements/wordpress/srcs/setup.sh
@@ -1,8 +1,8 @@
 service php7.3-fpm start
 service php7.3-fpm stop
 
-cp /tmp/html /var/www/
-cp /tmp/wp-config.php /var/www/html/
+cp -r /tmp/html/wordpress /var/www/html/
+cp /tmp/wp-config.php /var/www/html/wordpress/
 #cp /tmp/wp-config.php /var/www/html/wordpress/
 
 # Set permissions
@@ -13,7 +13,7 @@ chmod +x /usr/local/bin/wp
 # Install WordPress using non-root user
 useradd -m subuser
 chown -R subuser:subuser /var/www/html/
-runuser -l subuser -c 'wp core install --url="localhost/"  --title="tmorris Demo" --admin_user="admin" --admin_password="user42" --admin_email="example@example.com" --path=/var/www/html/'
+runuser -l subuser -c 'wp core install --url="localhost/wordpress/"  --title="tmorris Demo" --admin_user="admin" --admin_password="user42" --admin_email="example@example.com" --path=/var/www/html/wordpress/'
 
 php-fpm7.3 -F -R
 
