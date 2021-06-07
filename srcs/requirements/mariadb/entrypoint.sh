1178ef96efa311b562d12ac4b67515755304a326
tmorris <tmorris@student.42.fr>
2021-06-07 13:10:40 +0200

    Use custom network instead of the default
    Fix #1
    Fix #2

--- srcs/requirements/mariadb/entrypoint.sh
+++ srcs/requirements/mariadb/entrypoint.sh
@@ -5,8 +5,8 @@ if [ "$DB_EXISTS" = "0" ]; then
 fi
 USER_EXISTS=`mysql -Be "SELECT user FROM mysql.user;" | grep "$MYSQL_USER" | wc -l`;
 if [ "$USER_EXISTS" = "0" ]; then
-	mysql -e "CREATE USER '$MYSQL_USER'@'srcs_wordpress_1.srcs_default' IDENTIFIED BY '$MYSQL_PASSWORD';";
-	mysql -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE . * TO  '$MYSQL_USER'@'srcs_wordpress_1.srcs_default' IDENTIFIED BY '$MYSQL_PASSWORD';";
+	mysql -e "CREATE USER '$MYSQL_USER'@'srcs_wordpress_1.srcs_backend' IDENTIFIED BY '$MYSQL_PASSWORD';";
+	mysql -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE . * TO  '$MYSQL_USER'@'srcs_wordpress_1.srcs_backend' IDENTIFIED BY '$MYSQL_PASSWORD';";
 	mysql -e "FLUSH PRIVILEGES;";
 fi
 service mysql stop;
