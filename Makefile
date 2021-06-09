01bbabe7a579cd469c6ae3be65c2e8f2d5fff56d
tmorris <tmorris@student.42.fr>
2021-06-09 13:01:29 +0200

    Change project name to inception
    Re #18

--- Makefile
+++ Makefile
@@ -3,15 +3,15 @@ CONFIG_PATH = ./srcs/docker-compose.yml
 .DEFAULT_GOAL := all
 
 all:
-	docker-compose -f $(CONFIG_PATH) build
-	docker-compose -f $(CONFIG_PATH) up -d
+	docker-compose -p"inception" -f $(CONFIG_PATH) build
+	docker-compose -p"inception" -f $(CONFIG_PATH) up -d
 
 done:
-	docker-compose -f $(CONFIG_PATH) down
+	docker-compose -p"inception" -f $(CONFIG_PATH) down
 
 fclean: done
-	docker volume rm srcs_mariadb-db srcs_wp-test 
-	docker image rm my_wordpress_img my_mariadb_img my_nginx_img
+	docker volume rm inception_mariadb-db inception_wordpress-files 
+	docker image rm wordpress mariadb nginx
 
 re: fclean
 	$(MAKE) all
--- srcs/requirements/mariadb/entrypoint.sh
+++ srcs/requirements/mariadb/entrypoint.sh
@@ -5,8 +5,8 @@ if [ "$DB_EXISTS" = "0" ]; then
 fi
 USER_EXISTS=`mysql -Be "SELECT user FROM mysql.user;" | grep "$WORDPRESS_DB_USER" | wc -l`;
 if [ "$USER_EXISTS" = "0" ]; then
-	mysql -e "CREATE USER '$WORDPRESS_DB_USER'@'wordpress.srcs_backend' IDENTIFIED BY '$WORDPRESS_DB_PASSWORD';";
-	mysql -e "GRANT ALL PRIVILEGES ON $WORDPRESS_DB_NAME . * TO  '$WORDPRESS_DB_USER'@'wordpress.srcs_backend' IDENTIFIED BY '$WORDPRESS_DB_PASSWORD';";
+	mysql -e "CREATE USER '$WORDPRESS_DB_USER'@'wordpress.inception_backend' IDENTIFIED BY '$WORDPRESS_DB_PASSWORD';";
+	mysql -e "GRANT ALL PRIVILEGES ON $WORDPRESS_DB_NAME . * TO  '$WORDPRESS_DB_USER'@'wordpress.inception_backend' IDENTIFIED BY '$WORDPRESS_DB_PASSWORD';";
 	mysql -e "FLUSH PRIVILEGES;";
 fi
 service mysql stop;
